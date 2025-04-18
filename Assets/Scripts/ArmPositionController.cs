e554ccc9ad7661845b59b081764fa595ecad0a4e
Will Heitman <will@heit.mn>
2025-04-18 18:23:42 -0400

    Update arm position controller, ros arm interface, and pick and place scene

--- Assets/Scripts/ArmPositionController.cs
+++ Assets/Scripts/ArmPositionController.cs
@@ -2,202 +2,143 @@ using System;
 using Unity.Robotics;
 using Unity.Robotics.UrdfImporter.Control;
 using UnityEngine;
-using RosMessageTypes.Sensor;
-using System.Linq;
-using System.Collections.Generic;
-using UnityEngine.Assertions;
 
 
-public enum RotationDirection { None = 0, Positive = 1, Negative = -1 };
-public enum ControlType { PositionControl };
+// public enum RotationDirection { None = 0, Positive = 1, Negative = -1 };
 
 public class ArmPositionController : MonoBehaviour
 {
     private ArticulationBody[] articulationChain;
-    // Stores original colors of the part being highlighted
-    private Color[] prevColor;
     private int previousIndex;
-
-    [InspectorReadOnly(hideInEditMode: true)]
-    public string selectedJoint;
-    [HideInInspector]
-    public int selectedIndex;
-
-    public ControlType control = (ControlType)Unity.Robotics.UrdfImporter.Control.ControlType.PositionControl;
-    public float stiffness;
-    public float damping;
     public float forceLimit;
     public float speed = 5f; // Units: degree/s
     public float torque = 100f; // Units: Nm or N
     public float acceleration = 5f;// Units: m/s^2 / degree/s^2
 
-    [Range(-90, 90)]
-    public double jointAngle1;
-    [Range(-90, 90)]
-    public double jointAngle2;
-    [Range(-90, 90)]
-    public double jointAngle3;
-    [Range(-90, 90)]
-    public double jointAngle4;
-    [Range(-90, 90)]
-    public double jointAngle5;
-    [Range(-90, 90)]
-    public double jointAngle6;
-
-
-    [Tooltip("Color to highlight the currently selected join")]
-    public Color highLightColor = new Color(1.0f, 0, 0, 1.0f);
 
     void Start()
     {
-        previousIndex = selectedIndex = 1;
+        // previousIndex = selectedIndex = 1;
         this.gameObject.AddComponent<FKRobot>();
         articulationChain = this.GetComponentsInChildren<ArticulationBody>();
         int defDyanmicVal = 10;
         foreach (ArticulationBody joint in articulationChain)
         {
-            joint.gameObject.AddComponent<JointControl>();
+            // joint.gameObject.AddComponent<JointControl>();
             joint.jointFriction = defDyanmicVal;
             joint.angularDamping = defDyanmicVal;
             ArticulationDrive currentDrive = joint.xDrive;
+            currentDrive.stiffness = 10000.0f;
+            currentDrive.damping = 100.0f;
             currentDrive.forceLimit = forceLimit;
             joint.xDrive = currentDrive;
         }
+        // DisplaySelectedJoint(selectedIndex);
     }
 
-    void SetSelectedJointIndex(int index)
-    {
-        if (articulationChain.Length > 0)
-        {
-            selectedIndex = (index + articulationChain.Length) % articulationChain.Length;
-        }
-    }
 
     void Update()
     {
-        // bool SelectionInput1 = Input.GetKeyDown("right");
-        // bool SelectionInput2 = Input.GetKeyDown("left");
+        bool SelectionInput1 = Input.GetKeyDown("right");
+        bool SelectionInput2 = Input.GetKeyDown("left");
 
         // SetSelectedJointIndex(selectedIndex); // to make sure it is in the valid range
-
-        // // for (int i = 0; i < 6; i++)
-        // // {
-        // //     UpdateDirection(i);
-        // // }
+        // UpdateDirection(selectedIndex);
 
         // if (SelectionInput2)
         // {
         //     SetSelectedJointIndex(selectedIndex - 1);
-        //     Highlight(selectedIndex);
         // }
         // else if (SelectionInput1)
         // {
         //     SetSelectedJointIndex(selectedIndex + 1);
-        //     Highlight(selectedIndex);
         // }
 
         // UpdateDirection(selectedIndex);
+        // for (int i = 0; i < articulationChain.Length; i++)
+        // {
+        //     ArticulationBody joint = articulationChain[i];
+        //     ArticulationDrive xDrive = joint.xDrive;
+        //     xDrive.target = 1.5f;
+        //     Debug.Log($"Joint {joint.name} target: {xDrive.target}");
+
+        //     joint.xDrive = xDrive;
+        // }
     }
 
-    public JointStateMsg GetJointStates()
+    public void SetTargetAngles(double[] angles)
     {
-        JointStateMsg msg = new();
-
-        List<string> names = new List<string>();
-        List<double> positions = new List<double>();
-        List<double> velocities = new List<double>();
-        List<double> efforts = new List<double>();
-        for (int jointIndex = 0; jointIndex < articulationChain.Length; jointIndex++)
+        for (int i = 0; i < angles.Length; i++)
         {
 
-            JointControl current = articulationChain[jointIndex].GetComponent<JointControl>();
+            // Get the matching articulation body from the chain
+            for (int j = 0; j < articulationChain.Length; j++)
+            {
+                if (articulationChain[j].name == "link" + (i + 1))
+                {
+                    // Set the target angle for the joint
+                    ArticulationBody joint = articulationChain[j];
+                    ArticulationDrive xDrive = joint.xDrive;
+                    xDrive.target = (float)angles[i] * Mathf.Rad2Deg;
+                    joint.xDrive = xDrive;
+                    break;
+                }
+                if (j == articulationChain.Length - 1)
+                {
+                    Debug.LogWarning($"Joint with name 'link{i + 1}' not found in the articulation chain.");
+                }
+            }
 
-            // Debug.Log(current.joint.xDrive.target);
-            if (current.joint.dofCount != 1) continue; // Only consider joints with 1 DOF
-
-            positions.Add(current.joint.jointPosition[0]);
-
-            string name = current.name;
-            name = name.Replace("link", "joint");
-            names.Add(name);
-            // Debug.Log($"names.Count = {names.Count}");
-
-            velocities.Add(current.joint.jointVelocity[0]);
-            efforts.Add(current.joint.jointForce[0]);
-            // Debug.Log($"{current.name} = {current.joint.jointPosition[0]}");
-        }
-
-        Debug.Assert(names.Count == positions.Count, "Joint names and positions do not have same length");
-
-        if (positions.Count < 0)
-        {
-            Debug.Log("Positions was empty!");
-        }
-        if (names.Count < 0)
-        {
-            Debug.Log("Names was empty!");
-        }
-
-        msg.name = names.ToArray();
-
-        // Debug.Log($"msg.name.Length = {msg.name.Length}");
-
-        msg.position = positions.ToArray();
-        msg.velocity = velocities.ToArray();
-        msg.effort = efforts.ToArray();
-
-        return msg;
-    }
-
-    public void SetTargetAngles(double[] targetAngles)
-    {
-
-        // // Initialize six joint angles to zero
-        // targetAngles = new double[6] { 0, 22.6, 86.4, 7.3, -10.1, -16.3 };
-        // // targetAngles = new double[6] { 0, 0, 0, 0, 0, 0 };
-
-
-        // JointControl current = articulationChain[0].GetComponent<JointControl>();
-        // Debug.Log(current.joint.xDrive.target);
-        // ArticulationDrive drive = current.joint.xDrive;
-        // drive.target = (float)jointAngle1;
-        // current.joint.xDrive = drive;
-
-        // current = articulationChain[1].GetComponent<JointControl>();
-        // Debug.Log(current.joint.xDrive.target);
-        // drive = current.joint.xDrive;
-        // drive.target = (float)jointAngle2;
-        // current.joint.xDrive = drive;
-
-
-        for (int jointIndex = 0; jointIndex < targetAngles.Length; jointIndex++)
-        {
-
-            JointControl current = articulationChain[jointIndex + 1].GetComponent<JointControl>();
-
-            // Debug.Log(current.joint.xDrive.target);
-
-            ArticulationDrive drive = current.joint.xDrive;
-
-            // Debug.Log($"{current.name} = {targetAngles[jointIndex]}");
-
-            drive.target = (float)targetAngles[jointIndex] * Mathf.Rad2Deg;
-            current.joint.xDrive = drive;
-
-            // Debug.Log(current.joint.xDrive.target);
-            // UpdateDirection(jointIndex);
         }
     }
 
-    public void UpdateControlType(JointControl joint)
-    {
-        joint.controltype = (Unity.Robotics.UrdfImporter.Control.ControlType)control;
-        if (control == ControlType.PositionControl)
-        {
-            ArticulationDrive drive = joint.joint.xDrive;
-            drive.stiffness = stiffness;
-            drive.damping = damping;
-            joint.joint.xDrive = drive;
-        }
-    }
+
+    // /// <summary>
+    // /// Sets the direction of movement of the joint on every update
+    // /// </summary>
+    // /// <param name="jointIndex">Index of the link selected in the Articulation Chain</param>
+    // private void UpdateDirection(int jointIndex)
+    // {
+    //     if (jointIndex < 0 || jointIndex >= articulationChain.Length)
+    //     {
+    //         return;
+    //     }
+
+    //     float moveDirection = Input.GetAxis("Vertical");
+    //     JointControl current = articulationChain[jointIndex].GetComponent<JointControl>();
+    //     if (previousIndex != jointIndex)
+    //     {
+    //         JointControl previous = articulationChain[previousIndex].GetComponent<JointControl>();
+    //         previous.direction = RotationDirection.None;
+    //         previousIndex = jointIndex;
+    //     }
+
+    //     if (current.controltype != ControlType.PositionControl)
+    //     {
+    //         UpdateControlType(current);
+    //     }
+
+    //     // if (moveDirection > 0)
+    //     // {
+    //     //     current.direction = RotationDirection.Positive;
+    //     // }
+    //     // else if (moveDirection < 0)
+    //     // {
+    //     //     current.direction = RotationDirection.Negative;
+    //     // }
+    //     // else
+    //     // {
+    //     //     current.direction = RotationDirection.None;
+    //     // }
+
+
+    // }
+
+    // public void UpdateControlType(JointControl joint)
+    // {
+    //     ArticulationDrive drive = joint.joint.xDrive;
+    //     drive.stiffness = stiffness;
+    //     drive.damping = damping;
+    //     joint.joint.xDrive = drive;
+    // }
 }
--- Assets/Scripts/ROS/RosArmInterface.cs
+++ Assets/Scripts/ROS/RosArmInterface.cs
@@ -25,8 +25,8 @@ public class RosArmInterface : MonoBehaviour
 
     public float forceLimit;
 
-    // [SerializeField]
-    // Camera gameCamera;
+    [SerializeField]
+    Camera gameCamera;
 
     [SerializeField]
     BoxCollider gripperTriggerCollider;
@@ -37,6 +37,10 @@ public class RosArmInterface : MonoBehaviour
     ArmPositionController armPositionController;
 
     TimeMsg currentTime = new TimeMsg();
+
+    private GameObject clickIndicator;
+    private float indicatorDuration = 2.0f; // How long to show the indicator
+
 
     // Start is called before the first frame update
     void Start()
@@ -69,35 +73,49 @@ public class RosArmInterface : MonoBehaviour
 
         // Publish joint states at rate jointStatePublishRateHz
         InvokeRepeating("PublishJointStates", 0.01f, (float)1 / jointStatePublishRateHz);
-        // InvokeRepeating("GetMouseTarget", 0.01f, 0.1f);
+        InvokeRepeating("GetMouseTarget", 0.01f, 0.1f);
     }
 
-    // void GetMouseTarget()
-    // {
-    //     if (Input.GetMouseButton(0) == false) return;
+    void GetMouseTarget()
+    {
+        if (Input.GetMouseButton(0) == false) return;
 
-    //     Ray ray = gameCamera.ScreenPointToRay(Input.mousePosition);
+        Ray ray = gameCamera.ScreenPointToRay(Input.mousePosition);
 
-    //     RaycastHit hit;
+        RaycastHit hit;
 
-    //     LayerMask terrainMask = LayerMask.GetMask("Terrain");
+        LayerMask terrainMask = LayerMask.GetMask("Terrain");
 
-    //     if (Physics.Raycast(ray, out hit, terrainMask))
-    //     {
-    //         Debug.DrawLine(transform.position, hit.point, Color.green, 0.1f);
+        if (Physics.Raycast(ray, out hit, terrainMask))
+        {
 
-    //         // Debug.Log(hit.point);
-    //         Vector3 localPoint = transform.InverseTransformPoint(hit.point);
-    //         // localPoint.z = 0.0f;
-    //         // Debug.Log($"localPoint = {localPoint}");
+            // Draw an arrow at the hit point using the normal
+            Vector3 arrowStart = hit.point;
+            Vector3 arrowEnd = hit.point + hit.normal * 0.2f; // Small arrow along the normal
+            Debug.DrawLine(arrowStart, arrowEnd, Color.red, 0.1f);
 
-    //         // Send to ROS
-    //         PointMsg msg = localPoint.To<FLU>();
-    //         m_Ros.Publish("/ecosim/clicked_point", msg);
-    //     }
+            // Draw small lines to create arrowhead
+            Vector3 right = Vector3.Cross(hit.normal, Vector3.up).normalized * 0.05f;
+            if (right.magnitude < 0.01f) // If normal is parallel to up
+                right = Vector3.Cross(hit.normal, Vector3.forward).normalized * 0.05f;
+            Vector3 up = Vector3.Cross(hit.normal, right).normalized * 0.05f;
+            Debug.DrawLine(arrowEnd, arrowEnd - hit.normal * 0.05f + right, Color.red, 0.1f);
+            Debug.DrawLine(arrowEnd, arrowEnd - hit.normal * 0.05f - right, Color.red, 0.1f);
+            Debug.DrawLine(arrowEnd, arrowEnd - hit.normal * 0.05f + up, Color.red, 0.1f);
+            Debug.DrawLine(arrowEnd, arrowEnd - hit.normal * 0.05f - up, Color.red, 0.1f);
+
+            // Debug.Log(hit.point);
+            Vector3 localPoint = transform.InverseTransformPoint(hit.point);
+            // localPoint.z = 0.0f;
+            // Debug.Log($"localPoint = {localPoint}");
+
+            // Send to ROS
+            PointMsg msg = localPoint.To<FLU>();
+            m_Ros.Publish("/ecosim/clicked_point", msg);
+        }
 
 
-    // }
+    }
 
     void TransformCb(TFMessageMsg tfMessage)
     {
@@ -117,6 +135,7 @@ public class RosArmInterface : MonoBehaviour
             // Debug.Log(jointCommand.name[i] + " " + jointCommand.position[i]);
             int jointIndex = jointCommand.name[i][jointCommand.name[i].Length - 1] - '0';
             angles[jointIndex - 1] = (float)jointCommand.position[i];
+            Debug.Log($"Joint {jointIndex} angle: {angles[jointIndex - 1]}");
         }
 
 
@@ -127,11 +146,11 @@ public class RosArmInterface : MonoBehaviour
 
     void PublishJointStates()
     {
-        JointStateMsg msg = armPositionController.GetJointStates();
+        // JointStateMsg msg = armPositionController.GetJointStates();
         // JointStateMsg msg = new();
-        msg.header.stamp = currentTime;
+        // msg.header.stamp = currentTime;
 
-        m_Ros.Publish("/arm_joint_states", msg);
+        // m_Ros.Publish("/arm_joint_states", msg);
     }
 
     // Update is called once per frame
